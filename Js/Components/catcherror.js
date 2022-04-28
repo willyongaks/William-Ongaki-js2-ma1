2f5dc91a53bfbd6d5626ba0617465e73adecce13
willyongaks <william.ongaki10@gmail.com>
2022-04-28 12:30:15 +0200

    b

--- Js/Components/catcherror.js
+++ Js/Components/catcherror.js
@@ -1,3 +1,3 @@
-function catchError(message){
-    
-}
+// export function catchError(message){
+
+// }
--- Js/script.js
+++ Js/script.js
@@ -1,6 +1,10 @@
+// import catchError from "./Components/catcherror.js";
+const resultHtml = document.querySelector(".result");
+const Search = document.querySelector(".filter").value;
+
 const url = "https://fakestoreapi.com/products";
 
-const resultHtml = document.querySelector(".result");
+
 
 async function fakeStoreApi() {
     try {
@@ -14,8 +18,15 @@ async function fakeStoreApi() {
         for (let i = 0; i < result.length; i++) {
 
             resultHtml.innerHTML += `
-                                    <div>${result[i].title}</div>
-                                    <div>${result[i].price}</div>`
+                                    <div class="card">
+                                    <div>
+                                        <img src="https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg" alt="">
+                                    </div>
+                                    <div class="card-text">${result[i].title}</div>
+                                    <div class="card-text">${result[i].price}</div>
+                                    </div>`
+
+
         }
 
 
@@ -27,3 +38,21 @@ async function fakeStoreApi() {
 }
 
 fakeStoreApi();
+
+Search.onkeyup = function() {
+    console.log(Event);
+
+    const searchvalue = Event.target(search).value.trim();
+
+    const pricedfilter = results.filter(function (result) {
+        if (result.price[i].startWith(searchvalue)) {
+            return true;
+        }
+    });
+
+    console.log(pricedfilter);
+
+    result = pricedfilter;
+
+    fakeStoreApi();
+};
